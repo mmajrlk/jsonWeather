12e337184b7347277486630c177028fc145e7089
Mark Lalumondier <mlalumondier@gmail.com>
2016-03-20 17:17:09 -0500

    added weather icons and weather output

--- js/weather.js
+++ js/weather.js
@@ -1,4 +1,7 @@
 var weatherAPI = '6b21291040346db93ca363242f3dfc26';
+var weatherURL = 'http://api.openweathermap.org/data/2.5/weather';
+var weatherUnits;
+var weatherSuffix;
 
 //Hide JavaScript Error message
 
@@ -29,6 +32,7 @@ $('.locationGPS').on('click', function() {
 
 function gpsSuccess (position){
 	var locationData = [position.coords.latitude, position.coords.longitude];
+	weatherUnits = $('input:radio[name=measureSystem]:checked').val();
 	//Sucess run findWeather([lat, long], true) with corrd
 	findWeather(locationData, true);
 }
@@ -48,7 +52,7 @@ function gpsFail (message){
 //On submit button click
 $('.locationZipcodeSubmit').on('click', function() {
 	var zipcode = $('.locationZipcode').val();
-	console.log(zipcode);
+	weatherUnits = $('input:radio[name=measureSystem]:checked').val();
 	//Check to see if anything has been entered in the box
 	if (zipcode) {
 		//Sucess run findWeather([code], false) with zip
@@ -63,24 +67,30 @@ $('.locationZipcodeSubmit').on('click', function() {
 //fucntion findWeather (locationData, GPS)
 function findWeather (locationData, GPS) {
 
-	var weatherURL;
 	//hide form and display loading text
 
 	//If GPS
 	if (GPS) {
 		//store lat and long in a var
-		var gpsLat = locationData[0];
-		var gpsLong = locationData[1];
-		weatherURL = 'http://api.openweathermap.org/data/2.5/weather?lat='+gpsLat+'&lon='+gpsLong+'&APPID='+weatherAPI;
+		var weatherAPIData = {
+			lat: locationData[0],
+			lon: locationData[1],
+			units: weatherUnits,
+			APPID: weatherAPI
+		}
 
 	} else {
 		//add that var to the weather api and send
-		weatherURL = 'http://api.openweathermap.org/data/2.5/weather?zip='+locationData+',us&APPID='+weatherAPI;
+		var weatherAPIData = {
+			zip: locationData+',us',
+			units: weatherUnits,
+			APPID: weatherAPI
+		}
 	}
 
 	console.log(weatherURL);
 
-	$.post(weatherURL, function(data) {
+	$.getJSON(weatherURL, weatherAPIData, function(data) {
 			printWeather(data);
 		}).fail( function(){
 			//hide loading text
@@ -93,19 +103,26 @@ function findWeather (locationData, GPS) {
 }
 
 function printWeather(data) {
+	setSuffix();
 	console.log(data);
 	//hide form fields
-	//parse JSON data
-	//get weather icon and translate to our iconset
+	//get weather icon and translate to our icon set
+	var weatherIcon = 'wi wi-owm-' + data.weather[0].id;
 	//build out each part of weather data
-$('.locationName').text(data.name);
-$('.coord').text('(lon: '+data.coord.log+' lat: '+data.coord.lat+')');
-$('.description .data').text(data.name);
-$('.currentTemp .data').text(data.name);
-$('.maxMinTemp .data').text(data.name);
-$('.wind .data').text(data.name);
-$('.rainSnow .data').text(data.name);
-$('.updateTime .data').text(data.name);
+	$('i.wi').removeClass().addClass(weatherIcon);
+	$('.locationName').text(data.name);
+	$('.coord .data0').text(data.coord.lon+weatherSuffix.none);
+	$('.coord .data1').text(data.coord.lat+weatherSuffix.none);
+	$('.description .data0').text(data.weather[0].main);
+	$('.description .data1').text(data.weather[0].description);
+	$('.currentTemp .data0').text(data.main.temp+weatherSuffix.temp);
+	$('.maxMinTemp .data0').text(data.main.temp_max+weatherSuffix.temp);
+	$('.maxMinTemp .data1').text(data.main.temp_min+weatherSuffix.temp);
+	$('.wind .data0').text(data.wind.speed+weatherSuffix.dist);
+	$('.wind .data1').text(data.wind.deg);
+	if (data.rain){$('.rainAmount .data0').text(data.rain['1h']+weatherSuffix.amnt);}else{$('.rainAmount .data0').text('0');}
+	if (data.snow){$('.snowAmount .data0').text(data.snow['1h']+weatherSuffix.amnt);}else{$('.snowAmount .data0').text('0');}
+	$('.updateTime .data0').text(new Date(data.dt).toLocaleTimeString());
 
 	//print data to weather container
 	//add update or new location below weather container
@@ -114,5 +131,24 @@ $('.updateTime .data').text(data.name);
 		//on new location click
 			//clear weather container and update/newlocaiton buttons
 			//display form fields
+}
+
+//Sets suffix based on units used.
+function setSuffix() {
+	if (weatherUnits === 'metric'){
+		weatherSuffix = {
+			none: '\xB0',
+			temp: '\xB0C',
+			dist: 'kph',
+			amnt: 'cm'
+		}
+	} else {
+		weatherSuffix = {
+			none: '\xB0',
+			temp: '\xB0F',
+			dist: 'mph',
+			amnt: 'in'
+		}
+	}
 }
 
